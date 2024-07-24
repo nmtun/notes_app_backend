3b43789cb8feb1749575662164e8ffb08a44c166
Nguyen Manh Tung <163404922+nmtun@users.noreply.github.com>
2024-07-24 23:18:42 +0700

    update search-note api

--- index.js
+++ index.js
@@ -292,6 +292,36 @@ app.put("/update-note-pinned/:noteId", authenticateToken, async(req, res) => {
     } 
 })
 
+//Search note
+app.get("/search-notes", authenticateToken, async (req, res) => {
+    const { user } = req.user;
+    const { query } = req.query;
+
+    if (!query) {
+        return res.status(400).json({ error: true, message: "Search query is required" });
+    }
+
+    try {
+        const matchingNotes = await Note.find({
+            userId: user._id,
+            $or: [
+                { title: { $regex: new RegExp(query, "i") } },
+                { content: { $regex: new RegExp(query, "i") } }
+            ]
+        });
+        return res.json({
+            error: false,
+            notes: matchingNotes,
+            message: "Notes matching the search query retrieved successfully"
+        });
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: "Internal Server Error"
+        });
+    }
+});
+
 app.listen(8000)
 
 module.exports = app
